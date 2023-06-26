b864c79ab0bdb4da975fb6b33547b3de99c88545
david444456 <david-toranzo-@hotmail.com>
2023-06-26 00:16:53 -0300

    [A] add start installer

---
+++ Runtime/Installers/Installers/StartInstallerInitial.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Toranzo.Installer
+{
+    public class StartInstallerInitial : MonoBehaviour
+    {
+        [SerializeField] private MonoInstaller _monoInstaller;
+
+        private void Start()
+        {
+            _monoInstaller.Install();
+        }
+    }
+}
